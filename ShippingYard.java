0ac79d0ef914201220e16bf7ba6396f9a378c500
Omid <omid.alemi@gmail.com>
2011-03-22 19:09:47 -0700

    Basic env

---
+++ ShippingYard.java
@@ -0,0 +1,39 @@
+// Environment code for project CargoWorld.mas2j
+
+import jason.asSyntax.*;
+import jason.environment.*;
+import java.util.logging.*;
+
+public class ShippingYard extends Environment {
+
+    private Logger logger = Logger.getLogger("CargoWorld.mas2j."+ShippingYard.class.getName());
+
+    /** Called before the MAS execution with the args informed in .mas2j */
+    @Override
+    public void init(String[] args) {
+        super.init(args);
+        
+    }
+
+    @Override
+    public boolean executeAction(String agName, Structure action) {
+       		
+		if (action.getFunctor().equals("move")) {
+			//to-do
+			return true;
+		}
+		else
+		{
+			logger.info("executing: "+action+", but not implemented!");
+		}
+		
+        return true;
+    }
+
+    /** Called before the end of MAS execution */
+    @Override
+    public void stop() {
+        super.stop();
+    }
+}
+
